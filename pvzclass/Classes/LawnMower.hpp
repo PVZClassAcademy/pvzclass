82afa65e68116bdcc2c72d39e819ac1f3b831ca7
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2026-01-29 12:05:04 +0800

    refactor!: GetByIndex Part 3

--- pvzclass/Classes/LawnMower.hpp
+++ pvzclass/Classes/LawnMower.hpp
@@ -15,7 +15,11 @@ namespace PVZ
 		///		若派生类需要对应扩指针的对象，请在派生类中修改此数值。
 		static DWORD MemSize;
 		/// @attention 从 3.0 起，该函数将不再具有按编号构造的功能。
-		LawnMower(int indexoraddress);
+		LawnMower(uint32_t address) : BaseClass(address) {};
+		/// @brief 获取指定编号的对象
+		/// @param index 编号
+		/// @note 不保证以此法获得的对象未被移除
+		static LawnMower GetByIndex(uint32_t index);
 		/// @brief 所在的 PVZApp
 		T_PROPERTY(PVZApp, App, __get_App, __set_App, 0);
 		/// @brief 所在的 Board
--- pvzclass/Classes/TodParticleSystem.hpp
+++ pvzclass/Classes/TodParticleSystem.hpp
@@ -9,7 +9,11 @@ namespace PVZ
 	{
 	public:
 		/// @attention 从 3.0 起，该函数将不再具有按编号构造的功能。
-		TodParticleSystem(DWORD indexoraddress);
+		TodParticleSystem(uint32_t address) : BaseClass(address) {};
+		/// @brief 获取指定编号的对象
+		/// @param index 编号
+		/// @note 不保证以此法获得的对象未被移除
+		static TodParticleSystem GetByIndex(uint32_t index);
 		/// @brief 是否已被移除。
 		T_PROPERTY(BOOLEAN, Dead, __get_Dead, __set_Dead, 0x1C);
 		T_PROPERTY(DWORD, Id, __get_Id, __set_Id, 0x28);
