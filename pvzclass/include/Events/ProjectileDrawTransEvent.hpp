b3c363ba8677048777f239c803692e9b935b2dc6
quminzhennna <192948673@qq.com>
2026-02-08 17:50:56 +0800

    fix(ProjectileDrawTransEvent):wrong hook addr

--- pvzclass/include/Events/ProjectileDrawTransEvent.hpp
+++ pvzclass/include/Events/ProjectileDrawTransEvent.hpp
@@ -5,7 +5,7 @@ namespace PVZEvent
 {
 	/// @brief 子弹绘制贴图变换矩阵后事件
 	/// @param Matrix3
-	class ProjectileDrawTransEvent : public DLLEventTemplate<0x46E7F6, 5, REG_ESI>
+	class ProjectileDrawTransEvent : public DLLEventTemplate<0x46E7FB, 6, REG_ESI>
 	{
 	public:
 		ProjectileDrawTransEvent(const char* str) : DLLEventTemplate() { Init(str); };
