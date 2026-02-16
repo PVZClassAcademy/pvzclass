20274b351af3b7dfe8ec8a5bde7e20337b55542f
quminzhennan <192948673@qq.com>
2026-02-16 15:31:22 +0800

    feat(PVZ::CutSence):CancelIntro

--- pvzclass/Classes/CutScene.hpp
+++ pvzclass/Classes/CutScene.hpp
@@ -7,8 +7,22 @@ namespace PVZ
 	{
 	public:
 		CutScene(int address);
+		INT_SIMPLE_PROPERTY(mCutsceneTime, 0x08);
+		INT_SIMPLE_PROPERTY(mSodTime, 0x0C);
+		INT_SIMPLE_PROPERTY(mGraveStoneTime, 0x10);
+		INT_SIMPLE_PROPERTY(mReadySetPlantTime, 0x14);
+		INT_SIMPLE_PROPERTY(mFogTime, 0x18);
+		INT_SIMPLE_PROPERTY(mBossTime, 0x1C);
+		INT_SIMPLE_PROPERTY(mCrazyDaveTime, 0x20);
+		INT_SIMPLE_PROPERTY(mLawnMowerTime, 0x24);
+		INT_SIMPLE_PROPERTY(mCrazyDaveDialogStart, 0x28);
+
+		T_SIMPLE_PROPERTY(bool, mSeedChoosing, 0x2C);
+		T_SIMPLE_PROPERTY(bool, mPreloaded, 0x34);
 		/// @brief 是否已经放置街上的僵尸
-		T_PROPERTY(BOOLEAN, PlacedZombies, __get_PlacedZombies, __set_PlacedZombies, 0x35);
+		T_SIMPLE_PROPERTY(bool, mPlacedZombies, 0x35);
+		T_SIMPLE_PROPERTY(bool, mPlacedLawnItems, 0x36);
+
 		/// @brief 放置街上的僵尸
 		/// @note x 和 y 是街上的坐标，不是场地中的格子坐标
 		/// @note x 和 y 的取值应该是 0,1,2,3,4
@@ -19,5 +33,7 @@ namespace PVZ
 		/// @param grid 该格子是否已被占用
 		/// @note grid 数组会被实时修改更新
 		void PlaceStreetZombie(ZombieType::ZombieType type, bool grid[5][5]);
+		/// @brief 取消过场动画
+		void CancelIntro();
 	};
 }
