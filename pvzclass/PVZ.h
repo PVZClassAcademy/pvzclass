dfc30bd14fe9bfbb138be61260a759b56c5d7749
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2026-04-03 09:29:51 +0800

    feat(AttachEffect): extend

--- pvzclass/PVZ.h
+++ pvzclass/PVZ.h
@@ -48,6 +48,19 @@ using std::is_base_of;
 
 inline constexpr auto INVALID_BASEADDRESS = 0x400000;
 
+namespace PVZEnum
+{
+	/// @brief 附件效果类型
+	enum class AttachEffectType : int
+	{
+		PARTICLE,
+		TRAIL,
+		REANIM,
+		ATTACHMENT,
+		OTHER
+	};
+}
+
 /// @brief 包含大部分用于控制 PVZ 内部对象的类和方法。
 /// @note Only version 1.0.0.1051 is fully supported
 namespace PVZ
@@ -367,11 +380,21 @@ namespace PVZ
 		void ScaleRotateTransformMatrix(float x, float y, float rad, float ScaleX, float ScaleY);
 	};
 	class TrackInstance;
+
+	/// @brief 附件效果，为 Attachment 的主要成员
 	class AttachEffect : public BaseClass
 	{
 	public:
-		AttachEffect(int address) : BaseClass(address) {};
+		AttachEffect(uint32_t address) : BaseClass(address) {};
+		/// @brief 附件效果识别 ID
+		INT_SIMPLE_PROPERTY(EffectID, 0);
+		/// @brief 附件效果类型
+		T_SIMPLE_PROPERTY(PVZEnum::AttachEffectType, Type, 4);
+		/// @brief 获取附件效果的变换矩阵
+		/// @return 变换矩阵
 		Matrix3 GetOffset();
+		T_SIMPLE_PROPERTY(uint8_t, DontDrawIfParentHidden, 0x2C);
+		T_SIMPLE_PROPERTY(uint8_t, DontPropogateColor, 0x2D);
 	};
 	/// @brief 动画部件。相当于其他游戏的模型。
 	class Animation : public BaseClass
