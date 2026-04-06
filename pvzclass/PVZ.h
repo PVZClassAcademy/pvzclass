901a7d2f4dda416aca427677c3d48b221e15ae12
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2026-04-06 09:28:10 +0800

    docs: mark mainwindowhandle related files deprecated

--- pvzclass/PVZ.h
+++ pvzclass/PVZ.h
@@ -567,9 +567,11 @@ namespace PVZ
 		INT_PROPERTY(X, __get_X, __set_X, 0xE0);
 		INT_PROPERTY(Y, __get_Y, __set_Y, 0xE4);
 		T_READONLY_PROPERTY(MouseClickState::MouseClickState, ClickState, __get_ClickState, 0xE8);
-		void WMClick(short x, short y);
+		/// @deprecated 即将被移除
+		[[deprecated]] void WMClick(short x, short y);
 		void GameClick(int x, int y);
-		void MoveTo(int x, int y);
+		/// @deprecated 即将被移除
+		[[deprecated]] void MoveTo(int x, int y);
 	};
 	class GardenPlant : public BaseClass
 	{
