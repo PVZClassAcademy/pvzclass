58b542537c93a1160256168fcb4ec2acd43ffc12
quminzhennan <125654294+qum1nzhennan@users.noreply.github.com>
2026-06-30 06:32:22 +0800

    #include路径修复 (#173)
    * fix(PVZ::Zombie::GetBobsledPosition):save the return value
    
    * fix: update const-correctness in Classes.hpp, SaveGameContext, ButtonWidget, Dialog

--- pvzclass/Classes.hpp
+++ pvzclass/Classes.hpp
@@ -29,7 +29,7 @@
 
 #include "include/Effects/EffectSystem.hpp"
 
-#include "Widgets/ButtonWidget.hpp"
-#include "Widgets/Dialog.hpp"
+#include "include/Widgets/ButtonWidget.hpp"
+#include "include/Widgets/Dialog.hpp"
 #include "include/Widgets/GameSelector.hpp"
 #include "include/Widgets/WidgetManager.hpp"
