7c8c2edccd02f888f8b7e3b0e220797ee99f5271
Violet Nonbloosom Reborn <violetnonbloosom@yeah.net>
2026-09-06 11:58:30 +0800

    fix(Event): PlayerInfoResetAfterEvent no effect

--- pvzclass/include/Events/PlayerInfoEvents.hpp
+++ pvzclass/include/Events/PlayerInfoEvents.hpp
@@ -6,12 +6,27 @@ namespace PVZEvent
 	/// @brief PlayerInfo 重置完成事件
 	/// @param 触发事件的 PlayerInfo
 	/// @note PlayerInfo 在创建时会进行重置，因此也会触发该事件。
-	/// @return 是否使用原版的描述文本
-	class PlayerInfoResetAfterEvent : public DLLEventTemplate<0x469A55, 5, REG_ESI>
+	class PlayerInfoResetAfterEvent : public DLLEvent 
 	{
 	public:
-		PlayerInfoResetAfterEvent(int address) : DLLEventTemplate() { Init(address); };
-		PlayerInfoResetAfterEvent(const char* name) : DLLEventTemplate() { Init(name); };
 		PlayerInfoResetAfterEvent() : PlayerInfoResetAfterEvent("onPlayerInfoResetAfter") {};
+		PlayerInfoResetAfterEvent(const char* str) : PlayerInfoResetAfterEvent(PVZ::Memory::GetProcAddress(str)) {};
+		PlayerInfoResetAfterEvent(int address)
+		{
+			hookAddress = 0x4699F8;
+			rawlen = 6;
+			BYTE code[] =
+			{
+				MOV_PTR_EUX_ADD__EVX(REG_ESI, REG_EBX, 0x350),
+				PUSH_ESI,
+				INVOKE(address),
+				ADD_ESP(4),
+
+				POPAD,
+				MOV_ECX(0x4699FE),
+				JMP_REG32(REG_ECX)
+			};
+			start(STRING(code));
+		}
 	};
 }
