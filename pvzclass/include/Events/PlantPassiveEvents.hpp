8250a37e30e1874c6cabf89a1fc3a52cf9796c48
Violet-Nonbloosom-Reborn <violetnonbloosom@yeah.net>
2026-09-18 17:14:29 +0800

    feat(Event): PlantDropRemainingSunEvent_ts

--- pvzclass/include/Events/PlantPassiveEvents.hpp
+++ pvzclass/include/Events/PlantPassiveEvents.hpp
@@ -13,4 +13,39 @@ namespace PVZEvent
 		CheckUmbrellaEvent_ts(int address) : ThreeStateEventTemplate() { Init(address); };
 		CheckUmbrellaEvent_ts() : CheckUmbrellaEvent_ts("IsUmbrellaPlant") {};
 	};
+
+	/// @brief 植物在 IZ 中判断是否掉落阳光事件
+	/// @param 触发事件的 Challenge，触发事件的植物
+	/// @note 原版的非 IZ 模式不触发该事件，但手动调用 Challenge::IZPlantDropRemainingSun() 可以触发此事件。
+	/// @return 由 ThreeSteate::ThreeState 表示的，植物是否掉落阳光
+	class PlantDropRemainingSunEvent_ts : public DLLEventTemplate<0x42B9D0, 5, REG_ESI, MEM_ESP_ADD(0x28)>
+	{
+	protected:
+		static constexpr std::array<uint8_t, 24> compiled_special_bytes =
+		{
+			TEST_EUX_EVX(REG_EAX, REG_EAX),
+			JS(15),
+			POPAD,
+			JE(16),
+
+			PUSH_EBX,
+			PUSH_EBP,
+			MOV_REG32_PTR_ESP_ADD_V(REG_EBP, 0x0C),
+			PUSH_EDI,
+			PUSHDWORD(0x42B9DD),
+			RET,
+
+			RETN(4)
+		};
+		virtual void InitExtra(AsmBuilder& builder)
+		{
+			builder.add_bytes(compiled_special_bytes.data(), 24);
+		}
+	public:
+		PlantDropRemainingSunEvent_ts(const char* str) : DLLEventTemplate() { Init(str); };
+		PlantDropRemainingSunEvent_ts(int address) : DLLEventTemplate() { Init(address); };
+		PlantDropRemainingSunEvent_ts() : PlantDropRemainingSunEvent_ts("onPlantDropRemainingSun") {};
+	};
+
+	class PlantDropRemainingSunParamEvent : DLLEvent
 }
